8805a71c690dda2c0d763198591a4026a96eb967
saad.elzwawy <saad.elzwawy@gastro-hero.de>
2021-12-26 13:39:34 +0100

    Der Webhook existiert. Die Daten werden persistiert. Ein resposives, Flexbox nutzendes Frontend existiert.

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'dashboard')]
-    public function index(): Response
+    public const DASHBOARD_UPDATE = '1e9';
+
+    /**
+     * @Route("/dashboard", name="dashboard", methods={"GET"})
+     * @throws \Psr\Cache\InvalidArgumentException
+     */
+    public function index(CacheInterface $cache): Response
     {
+        $dashboardData = json_decode($cache->get('dashboard_1e9', function (){})) ?? [];
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'data' => $dashboardData,
         ]);
     }
 }
--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -3,16 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mercure\PublisherInterface;
+use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class WebhookController extends AbstractController
 {
-    #[Route('/webhook', name: 'webhook')]
-    public function index(): Response
+    /**
+     * @Route("/webhook/{data}", name="webhook", methods={"POST","GET"})
+     * @throws \Psr\Cache\InvalidArgumentException
+     */
+    public function index(PublisherInterface $publisher, $data, Request $request, CacheInterface $cache): Response
     {
-        return $this->render('webhook/index.html.twig', [
-            'controller_name' => 'WebhookController',
-        ]);
+        $dashboardData = $request->getContent();
+        $cache->delete('dashboard_1e9');
+        $dashboardData = $cache->get('dashboard_1e9', function () use ($dashboardData) {
+            return $dashboardData;
+        });
+       $publisher(new Update($data, $dashboardData));
+        return new Response('success');
     }
 }
